3d9c9725670e993da8b8f8d4c80ad45d87bdedea
mudbri <mubashiranwar@live.com>
2020-04-17 20:15:25 +0500

    Fixed bugs in loading singedin links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,28 @@
 // Since this component has no state, this will be a functional component rather than a class component
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SignedInUserLinks from './SignedInUserLinks';
-import SignedInCompanyLinks from './SignedInCompanyLinks';
-import GuestUserLinks from './GuestUserLinks';
-import { connect } from 'react-redux'; // note: we do not need firebaseConnect here since we do not need to be in connect to firestore. We need to just connect to our redux state
-import logo from '../../logo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import SignedInUserLinks from "./SignedInUserLinks";
+import SignedInCompanyLinks from "./SignedInCompanyLinks";
+import GuestUserLinks from "./GuestUserLinks";
+import { connect } from "react-redux"; // note: we do not need firebaseConnect here since we do not need to be in connect to firestore. We need to just connect to our redux state
+import logo from "../../logo.png";
 
 const Navbar = (props) => {
   const { auth, profile } = props;
-  
+
   let links = <GuestUserLinks />;
 
   if (auth.uid && profile.currProfile) {
-    if (profile.currProfile.type === 'Company') {
+    console.log("ASD");
+    if (profile.currProfile.type === "Company") {
       links = <SignedInCompanyLinks profile={profile.currProfile} />;
     } else {
       links = <SignedInUserLinks profile={profile.currProfile} />;
     }
   }
-
+  console.log("Links", links);
   return (
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" class="navbar-brand">
         <img src={logo} className="brand-logo left logo" />
@@ -39,7 +39,6 @@ const Navbar = (props) => {
         <span class="navbar-toggler-icon"></span>
       </button>
       {links}
-
     </nav>
   );
 };
@@ -53,4 +52,3 @@ const mapStateToProps = (state) => {
 
 // Exporting to be used in app.js
 export default connect(mapStateToProps)(Navbar);
-
