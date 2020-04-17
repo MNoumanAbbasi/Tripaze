db3c995f092c97326cd463d4efea2a455994398d
Cypharris <harisnoor674@gmail.com>
2020-04-17 18:51:44 +0500

    Integrated Navbars

--- src/components/layout/SignedInCompanyLinks.js
+++ src/components/layout/SignedInCompanyLinks.js
@@ -6,27 +6,45 @@ import { connect } from 'react-redux'
 import { signOut } from '../../store/actions/authActions'
 
 // this is a functional component, not a class, so we need to pass the props as argument
- const SignedInCompanyLinks = (props) => {
-     console.log(props)
-     const initial = props.profile? props.profile.companyName : null
-     return (
-        <ul className="right">
-            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>
-                {initial}
-            </NavLink></li>
-            <li><a onClick={props.signOut}>Sign Out</a></li>
-            <li><NavLink to='/createtrip'>Create Trip</NavLink></li>
-        </ul>              
+const SignedInCompanyLinks = (props) => {
+    console.log(props)
+    const initial = props.profile ? props.profile.companyName : null
+    return (
+        // <ul className="right">
+        //     <li><NavLink to='/' className='btn btn-floating pink lighten-1'>
+        //         {initial}
+        //     </NavLink></li>
+        //     <li><a onClick={props.signOut}>Sign Out</a></li>
+        //     <li><NavLink to='/createtrip'>Create Trip</NavLink></li>
+        // </ul>
 
-     )
- }
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav navbar-fonts ml-auto justify-content-end">
+                <li class="nav-item">
+                    <NavLink to="/reatetrip" class="nav-link">
+                        Create Trip
+                    </NavLink>
+                </li>
+                <li class="nav-item">
+                    <NavLink to="/" class="nav-link">
+                        {initial}
+                    </NavLink>
+                </li>
+                <li class="nav-item nav-link">
+                    <a onClick={props.signOut}>Sign Out</a>
+                </li>
+            </ul>
+        </div>
+
+    )
+}
 
 
- const mapDispatchToProps =  (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
     return {
         signOut: () => dispatch(signOut())
     }
- }
+}
 
- // Exporting to be used in app.js
- export default connect(null, mapDispatchToProps)(SignedInCompanyLinks) 
+// Exporting to be used in app.js
+export default connect(null, mapDispatchToProps)(SignedInCompanyLinks) 
