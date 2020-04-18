a14f68b7e0f349200a8a867f49dad82b8069f671
mudbri <mubashiranwar@live.com>
2020-04-18 17:30:38 +0500

    Integrated profileType helper function

--- src/components/trips/CreateTrip.js
+++ src/components/trips/CreateTrip.js
@@ -1,136 +1,139 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { createTrip } from '../../store/actions/tripActions'
-import { Redirect } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createTrip } from "../../store/actions/tripActions";
+import { Redirect } from "react-router-dom";
+import { profileType } from "../../Helpers";
 
 // all css are from the materialized CSS class
 export class CreateTrip extends Component {
-    state = {
-        title: '',
-        destinations: '',
-        departureLoc: '',
-        departureDate: '',
-        duration: 0,
-        price: 0,
-        capacity: 0,
-        description: '',
-        attraction: '',
-        image: ''
+  state = {
+    title: "",
+    destinations: "",
+    departureLoc: "",
+    departureDate: "",
+    duration: 0,
+    price: 0,
+    capacity: 0,
+    description: "",
+    attraction: "",
+    image: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      // store the input on form fields on the state
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    // dont want the default action of page being reloaded
+    e.preventDefault();
+    // console.log(this.state)
+
+    // calls the createTrip function in mapDispatchToProps which in turn calls dispatch with an action of createTrip that handles the asynch request. This request is then sent to the reducer for dispatch
+    this.props.createTrip(this.state, this.props.profile);
+    this.props.history.push("/");
+  };
+
+  render() {
+    const { auth, profile } = this.props;
+    if (profileType(auth, profile) !== "Company") {
+      return <Redirect to="/" />;
     }
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="gre-text text-darken-3">Create Trip</h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+          </div>
 
-    handleChange = (e) => {
-        this.setState({
-            // store the input on form fields on the state
-            [e.target.id]: e.target.value
-        })
-    }
-    
-    handleSubmit = (e) => {
-        // dont want the default action of page being reloaded
-        e.preventDefault();
-        // console.log(this.state)
-        
-        // calls the createTrip function in mapDispatchToProps which in turn calls dispatch with an action of createTrip that handles the asynch request. This request is then sent to the reducer for dispatch 
-        this.props.createTrip(this.state, this.props.profile.currProfile);
-        this.props.history.push('/');
-    }
+          <div className="input-field">
+            <label htmlFor="destinations">Destinations</label>
+            <input type="text" id="destinations" onChange={this.handleChange} />
+          </div>
 
-    
-    
-    render() {
+          <div className="input-field">
+            <label htmlFor="departureLoc">Departure Location</label>
+            <input type="text" id="departureLoc" onChange={this.handleChange} />
+          </div>
 
-        const { profile } = this.props;
-        const currProfile = profile.currProfile ? profile.currProfile : null;
-        const ans = !currProfile || currProfile.type !== "Company";
-        if (ans) {
-            return <Redirect to='/'/>
-        }
-        return (
-            <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="gre-text text-darken-3">Create Trip</h5>
-                    <div className="input-field">
-                        <label htmlFor="title">Title</label>
-                        <input type="text" id="title" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="departureDate">Departure Date</label>
+            <input
+              type="date"
+              id="departureDate"
+              onChange={this.handleChange}
+            />
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="destinations">Destinations</label>
-                        <input type="text" id="destinations" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="duration">Duration</label>
+            <input type="number" id="duration" onChange={this.handleChange} />
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="departureLoc">Departure Location</label>
-                        <input type="text" id="departureLoc" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="price">Price</label>
+            <input type="number" id="price" onChange={this.handleChange} />
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="departureDate">Departure Date</label>
-                        <input type="date" id="departureDate" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="capacity">Capacity</label>
+            <input type="number" id="capacity" onChange={this.handleChange} />
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="duration">Duration</label>
-                        <input type="number" id="duration" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              onChange={this.handleChange}
+              className="materialize-textarea"
+            ></textarea>
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="price">Price</label>
-                        <input type="number" id="price" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="attraction">Attractions</label>
+            <textarea
+              id="attraction"
+              onChange={this.handleChange}
+              className="materialize-textarea"
+            ></textarea>
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="capacity">Capacity</label>
-                        <input type="number" id="capacity" onChange={this.handleChange}/>
-                    </div>
+          <div className="input-field">
+            <label htmlFor="image">Drag-drattractionop or</label>
+            <button className="btn blue lighten-1 z-depth-1">Upload</button>
+          </div>
 
-                    <div className="input-field">
-                        <label htmlFor="description">Description</label>
-                        <textarea id="description" onChange={this.handleChange} className="materialize-textarea"></textarea>
-                    </div>
+          <div className="input-field">
+            <button className="btn blue lighten-1 z-depth-1">Submit</button>
+          </div>
+        </form>
 
-                    <div className="input-field">
-                        <label htmlFor="attraction">Attractions</label>
-                        <textarea id="attraction" onChange={this.handleChange} className="materialize-textarea"></textarea>
-                    </div>
-                   
-
-                    <div className="input-field">
-                        <label htmlFor="image">Drag-drattractionop or</label>
-                        <button className="btn blue lighten-1 z-depth-1">Upload</button>
-                    </div>
-
-                    <div className="input-field">
-                        <button className="btn blue lighten-1 z-depth-1">Submit</button>
-                    </div>
-
-                </form>
-
-
-                <div className="input-field">
-                    <button className="btn grey lighten-1 z-depth-1">Cancel</button>
-                </div>
-            </div>
-        )
-    }
+        <div className="input-field">
+          <button className="btn grey lighten-1 z-depth-1">Cancel</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
-    return {
-        auth: state.firebase.auth,
-        profile: state.auth
-    }
-}
+  console.log(state);
+  return {
+    auth: state.firebase.auth,
+    profile: state.auth.currProfile,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-      // so when we call props.createTrip, it's gonna perform a dispatch using the asynch middleware createTrip in src/store/actions
-      createTrip: (trip, profile) => dispatch(createTrip(trip, profile))
-    }
-  }
-  
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // so when we call props.createTrip, it's gonna perform a dispatch using the asynch middleware createTrip in src/store/actions
+    createTrip: (trip, profile) => dispatch(createTrip(trip, profile)),
+  };
+};
 
 // passing null since mapStateToProps = null
-export default connect(mapStateToProps, mapDispatchToProps)(CreateTrip)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateTrip);
