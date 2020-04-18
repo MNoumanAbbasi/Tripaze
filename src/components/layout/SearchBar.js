634bdd1bed91d84124e677318f4b67bf736da3c3
Cypharris <harisnoor674@gmail.com>
2020-04-18 20:16:39 +0500

    Captured input from the search bar

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -5,30 +5,28 @@ import { NavLink } from "react-router-dom";
 
 const SearchBar = () => {
   return (
-    <div class="navbar-nav input-group form-group w-50 ml-auto mr-auto ">
-      <form
-        onSubmit={(e) => {
-          console.log(e);
-        }}
-        className="white"
-      >
-        <input
-          class="form-control form-rounded "
-          type="search"
-          placeholder="Search trips..."
-          aria-label="Search"
-        />
+    <form
+      onSubmit={(e) => {
+        console.log(e);
+      }}
+      className="navbar-nav input-group form-group adjusted-form w-50 ml-auto mr-auto "
+    >
+      <input
+        class="form-control form-rounded "
+        type="text"
+        placeholder="Search trips..."
+        aria-label="Search"
+      />
 
-        <div class="input-group-append">
-          <button
-            class="btn btn-secondary change-color border-0 form-rounded"
-            type="button"
-          >
-            <i class="fa fa-search"></i>
-          </button>
-        </div>
-      </form>
-    </div>
+      <div class="input-group-append">
+        <button
+          class="btn btn-secondary change-color adjusted-btn border-0 form-rounded"
+          type="submit"
+        >
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+    </form>
   );
 };
 
