c520b8cd988c8336681abbaba2ae764f2b7498fb
Cypharris <harisnoor674@gmail.com>
2020-04-21 03:19:30 +0500

    Made the Sign-Up company page responsive

--- src/components/auth/SignUpCompany.js
+++ src/components/auth/SignUpCompany.js
@@ -34,36 +34,6 @@ export class SignUpCompany extends Component {
     }
 
     return (
-      // <div className="container">
-      //   <form onSubmit={this.handleSubmit} className="white">
-      //     <h5 className="gre-text text-darken-3">Sign Up as Company</h5>
-      //     <div className="input-field">
-      //       <label htmlFor="companyName">Company Name</label>
-      //       <input type="text" id="companyName" onChange={this.handleChange} />
-      //     </div>
-      //     <div className="input-field">
-      //       <label htmlFor="contact">Contact Number</label>
-      //       <input type="number" id="contact" onChange={this.handleChange} />
-      //     </div>
-      //     <div className="input-field">
-      //       <label htmlFor="email">Email</label>
-      //       <input type="email" id="email" onChange={this.handleChange} />
-      //     </div>
-      //     <div className="input-field">
-      //       <label htmlFor="password">Password</label>
-      //       <input type="password" id="password" onChange={this.handleChange} />
-      //     </div>
-      //     <div className="input-field">
-      //       <button className="btn blue lighten-1 z-depth-1">Sign Up</button>
-      //     </div>
-
-      //     {/* To display sign up error */}
-      //     <div className="red-text center">
-      //       {authError ? <p>{authError}</p> : null}
-      //     </div>
-      //   </form>
-      // </div>
-
       <div className="row m-0 signin-page ">
         <div className="ccol-lg-3 col-md-4 col-sm-6 form-margin">
           <form onSubmit={this.handleSubmit} className="change-font-opensans ">
