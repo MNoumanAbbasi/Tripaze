357039cea23ce394836cd287e1b9439a65c508e3
mudbri <mubashiranwar@live.com>
2020-04-25 04:18:28 +0500

    Changed date format when storing

--- src/store/actions/tripActions.js
+++ src/store/actions/tripActions.js
@@ -7,6 +7,13 @@ export const createTrip = (trip, currProfile) => {
 
     // grabbing user's id and profile
     const authorId = getState().firebase.auth.uid;
+
+    // Storing date as long names
+    const date = new Date(trip.departureDate).toDateString(); // Wed Apr 08 2020
+    const dateSplit = date.split(' ');
+    const newDate = dateSplit[1] + ' ' + dateSplit[2] + ', ' + dateSplit[3];
+    trip.departureDate = newDate;
+
     firestore
       .collection('Trips')
       .add({
@@ -30,6 +37,13 @@ export const editTrip = (trip, tripID) => {
     // this variable will have a reference to our database
     const firestore = getFirestore();
     delete trip.notUpdated; // TODO: Change this to a better method
+
+    // Storing date as long names
+    const date = new Date(trip.departureDate).toDateString(); // Wed Apr 08 2020
+    const dateSplit = date.split(' ');
+    const newDate = dateSplit[1] + ' ' + dateSplit[2] + ', ' + dateSplit[3];
+    trip.departureDate = newDate;
+
     firestore
       .collection('Trips')
       .doc(tripID)
