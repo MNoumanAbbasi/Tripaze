0cc543d8e4a5dff1ff8c5d388590bb4bb970bf93
mudbri <mubashiranwar@live.com>
2020-04-25 02:07:03 +0500

    Image loading issues fixed

--- src/components/trips/DisplayImage.js
+++ src/components/trips/DisplayImage.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import storage from '../../config/fbConfig';
+import coverPhoto from '../../Images/coverPhoto.jpg';
+import LoadingBox from '../dashboard/LoadingBox';
 
 class DisplayImage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       url: '',
+      complete: false,
     };
     this.getUrl = this.getUrl.bind(this);
   }
+
   getUrl = () => {
-    storage
-      .ref('tripImages')
-      .child(this.props.img)
-      .getDownloadURL()
-      .then((url) => {
-        this.setState({ url });
-      });
+    console.log('asd', this.props.img);
+    if (this.props.img === '') {
+      this.setState({ url: coverPhoto, complete: true });
+    } else {
+      storage
+        .ref('tripImages')
+        .child(this.props.img)
+        .getDownloadURL()
+        .then((url) => {
+          this.setState({ url, complete: true });
+        });
+    }
   };
-  render() {
+
+  componentDidMount() {
     this.getUrl();
-    if (this.props.page == 'details')
-      return <img src={this.state.url} className="w-100 backDrop"></img>;
-    else return <img class="card-img-top" src={this.state.url} alt="" />;
+  }
+
+  render() {
+    // this.getUrl();
+    if (this.state.complete) {
+      if (this.props.page == 'details')
+        return <img src={this.state.url} className="w-100 backDrop"></img>;
+      else return <img class="card-img-top" src={this.state.url} alt="" />;
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 
