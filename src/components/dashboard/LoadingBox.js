47f64aa9111329cb0c9a50d1033184eb60ae984f
Cypharris <harisnoor674@gmail.com>
2020-04-25 18:35:45 +0500

    Changed Margins

--- src/components/dashboard/LoadingBox.js
+++ src/components/dashboard/LoadingBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingBox = () => {
   return (
-    <div className="row signin-page">
+    <div className="row full-height-width">
       <div className=" col form-margin" style={{ height: '100px' }}>
         <div className="d-flex justify-content-center align-items-center ml-3">
           <i class="fa fa-3x fa-spinner fa-spin text-turq"></i>
