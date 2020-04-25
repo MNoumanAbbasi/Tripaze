47670dae986c45f7228696d01576f426e8b43a4e
Cypharris <harisnoor674@gmail.com>
2020-04-25 18:31:03 +0500

    Centered the loading div

--- src/components/dashboard/LoadingBox.js
+++ src/components/dashboard/LoadingBox.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const LoadingBox = () => {
   return (
-    <div
-      className="d-flex justify-content-center align-items-center"
-      style={{ height: '100px' }}
-    >
-      <i class="fa fa-3x fa-spinner fa-spin text-turq"></i>
-      <div className="m-3 home-heading">Loading</div>
+    <div className="row signin-page">
+      <div className=" col form-margin" style={{ height: '100px' }}>
+        <div className="d-flex justify-content-center align-items-center ml-3">
+          <i class="fa fa-3x fa-spinner fa-spin text-turq"></i>
+          <div className="m-3 home-heading">Loading</div>
+        </div>
+      </div>
     </div>
   );
 };
