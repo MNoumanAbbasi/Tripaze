d29c0541abe7acf8f25a352e5c1defb1ec0aecc6
Cypharris <harisnoor674@gmail.com>
2020-04-25 22:32:39 +0500

    Centered the loading screen

--- src/components/dashboard/LoadingBox.js
+++ src/components/dashboard/LoadingBox.js
@@ -2,30 +2,32 @@ import React from 'react';
 
 const LoadingBox = () => {
   return (
-    <div class="boxes">
-      <div class="box">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="box">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="box">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="box">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="loading-align">
+      <div class="boxes">
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
