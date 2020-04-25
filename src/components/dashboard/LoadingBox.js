76ea9b150c60b66b232139f7303a7da59087ee17
Cypharris <harisnoor674@gmail.com>
2020-04-25 18:36:45 +0500

    Fixed alignment issue

--- src/components/dashboard/LoadingBox.js
+++ src/components/dashboard/LoadingBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingBox = () => {
   return (
     <div className="row full-height-width">
-      <div className=" col form-margin" style={{ height: '100px' }}>
+      <div className=" col center-align" style={{ height: '100px' }}>
         <div className="d-flex justify-content-center align-items-center ml-3">
           <i class="fa fa-3x fa-spinner fa-spin text-turq"></i>
           <div className="m-3 home-heading">Loading</div>
