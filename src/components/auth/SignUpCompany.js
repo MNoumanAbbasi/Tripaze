4bbbbf637bdcd6d6dffc9720f7aa57d7e23462b6
Cypharris <harisnoor674@gmail.com>
2020-04-25 18:33:43 +0500

    Fixed alignment issue

--- src/components/auth/SignUpCompany.js
+++ src/components/auth/SignUpCompany.js
@@ -35,7 +35,7 @@ export class SignUpCompany extends Component {
 
     return (
       <div className="row m-0 full-height-width">
-        <div className="col-lg-3 col-md-4 col-sm-6 form-margin">
+        <div className="col-lg-3 col-md-4 col-sm-6 center-align">
           <form onSubmit={this.handleSubmit} className="change-font-opensans ">
             <h3 class="mt-20 mb-4 text-center">Sign Up as Company</h3>
             <div>
