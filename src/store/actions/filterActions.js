5a6daa381bb8b62a29a5c90b650824215871387e
mudbri <mubashiranwar@live.com>
2020-04-26 21:08:56 +0500

    Search bar visibility status passed to redux store

--- src/store/actions/filterActions.js
+++ src/store/actions/filterActions.js
@@ -8,13 +8,26 @@ export const search = (destination) => {
     // grabbing user's id and profile
     const authorId = getState().firebase.auth.uid;
     firestore
-      .collection("Trips")
-      .where([["destinations", "==", destination]])
+      .collection('Trips')
+      .where([['destinations', '==', destination]])
       .then((trips) => {
-        dispatch({ type: "SEARCH_TRIP", trips: trips });
+        dispatch({ type: 'SEARCH_TRIP', trips: trips });
       })
       .catch((err) => {
-        dispatch({ type: "SEARCH_TRIP_ERROR", err });
+        dispatch({ type: 'SEARCH_TRIP_ERROR', err });
       });
+  };
+};
+
+export const searchBarShow = (show) => {
+  // we want to return a function and halt the action dispatch until the function finishes
+  // dispatch is the funciton that dispatches an action to the reducer
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    // this variable will have a reference to our database
+    if (show) {
+      dispatch({ type: 'SHOW_SEARCH_BAR' });
+    } else {
+      dispatch({ type: 'HIDE_SEARCH_BAR' });
+    }
   };
 };
--- src/store/reducers/filterReducer.js
+++ src/store/reducers/filterReducer.js
@@ -1,17 +1,29 @@
 // for intial state when app starts
-const initState = { trips: null };
+const initState = { trips: null, showSearch: true };
 
 const filterReducer = (state = initState, action) => {
   switch (action.type) {
-    case "SEARCH_TRIP":
-      console.log("created trip", action.trip);
+    case 'SEARCH_TRIP':
+      console.log('created trip', action.trip);
       return {
         ...state,
         trips: action.trips,
       };
-    case "SEARCH_TRIP_ERROR":
-      console.log("created trip error", action.err);
+    case 'SEARCH_TRIP_ERROR':
+      console.log('created trip error', action.err);
       return state;
+    case 'SHOW_SEARCH_BAR':
+      console.log('search bar now showing');
+      return {
+        ...state,
+        showSearch: true,
+      };
+    case 'HIDE_SEARCH_BAR':
+      console.log('search bar now hiding');
+      return {
+        ...state,
+        showSearch: false,
+      };
     default:
       return state;
   }
