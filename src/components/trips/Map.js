736ae650d955efa0457b223fda825e3075cbf1cc
Taimoor Arif <taimoorrf@gmail.com>
2020-04-30 20:10:10 +0500

    Made map container

--- src/components/trips/Map.js
+++ src/components/trips/Map.js
@@ -1,21 +1,35 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-
-export class MapContainer extends Component {
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+ 
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow>
-      </Map>
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     );
   }
 }
-
-export default GoogleApiWrapper({
-  apiKey: AIzaSyAdrE2IZMrS6t9XpbPNN5gbHNwaGt2biNs,
-})(MapContainer);
+ 
+export default SimpleMap;
