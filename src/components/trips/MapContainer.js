20d47ad3acc9dcd71c62fc8e0849e8915725ff28
Taimoor Arif <taimoorrf@gmail.com>
2020-05-02 03:08:31 +0500

    Fixed bugs in Map container

--- src/components/trips/MapContainer.js
+++ src/components/trips/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import Geocode from 'react-geocode';
 
 const mapStyles = {
