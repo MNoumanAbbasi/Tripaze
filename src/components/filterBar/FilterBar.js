0faff5e8adc7dd710242d50fa8c01e6887ddb461
Cypharris <harisnoor674@gmail.com>
2020-05-02 18:38:11 +0500

    Custom dropdown menu

--- src/components/filterBar/FilterBar.js
+++ src/components/filterBar/FilterBar.js
@@ -87,6 +87,51 @@ class FilterBar extends Component {
     });
   };
 
+  // CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  //   <a
+  //     href=""
+  //     ref={ref}
+  //     onClick={(e) => {
+  //       e.preventDefault();
+  //       onClick(e);
+  //     }}
+  //   >
+  //     {children}
+  //     &#x25bc;
+  //   </a>
+  // ));
+
+  // // forwardRef again here!
+  // // Dropdown needs access to the DOM of the Menu to measure it
+  // CustomMenu = React.forwardRef(
+  //   ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+  //     const [value, setValue] = useState('');
+
+  //     return (
+  //       <div
+  //         ref={ref}
+  //         style={style}
+  //         className={className}
+  //         aria-labelledby={labeledBy}
+  //       >
+  //         <FormControl
+  //           autoFocus
+  //           className="mx-3 my-2 w-auto"
+  //           placeholder="Type to filter..."
+  //           onChange={(e) => setValue(e.target.value)}
+  //           value={value}
+  //         />
+  //         <ul className="list-unstyled">
+  //           {React.Children.toArray(children).filter(
+  //             (child) =>
+  //               !value || child.props.children.toLowerCase().startsWith(value)
+  //           )}
+  //         </ul>
+  //       </div>
+  //     );
+  //   }
+  // );
+
   render() {
     let destinations = [];
     this.props.trips.forEach((trip) => {
