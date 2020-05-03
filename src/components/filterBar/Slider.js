ece4e6d23a389eb0c5f59aa0cf9f071b4e570284
Cypharris <harisnoor674@gmail.com>
2020-05-04 02:30:51 +0500

    Removed input boxes and extra functions

--- src/components/filterBar/Slider.js
+++ src/components/filterBar/Slider.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Slider, InputNumber, Row, Col } from 'antd';
+import { Slider } from 'antd';
 import 'antd/dist/antd.css';
 
 class RangeSlider extends Component {
@@ -35,17 +35,6 @@ class RangeSlider extends Component {
   render() {
     const { inputValue } = this.state;
     return (
-      //   <Row>
-      //     <Col span={4}>
-      //       <InputNumber
-      //         min={this.minimum}
-      //         max={this.maximum}
-      //         style={{ margin: '0 16px' }}
-      //         value={inputValue[0]}
-      //         onChange={this.onChange1}
-      //       />
-      //     </Col>
-      // <Col span={15}>
       <Slider
         range
         onChange={this.onChange}
@@ -54,17 +43,6 @@ class RangeSlider extends Component {
         max={this.maximum}
         value={inputValue}
       />
-      // </Col>
-      //     <Col span={4}>
-      //       <InputNumber
-      //         min={this.minimum}
-      //         max={this.maximum}
-      //         style={{ margin: '0 16px' }}
-      //         value={inputValue[1]}
-      //         onChange={this.onChange}
-      //       />
-      //     </Col>
-      //   </Row>
     );
   }
 }
