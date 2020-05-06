c8a7ab5a6231cedb117ee75a27a3f64b1aeccb2a
Taimoor Arif <taimoorrf@gmail.com>
2020-05-06 21:05:03 +0500

    Fixed all inconsistencies.

--- src/components/companyProfile/ReviewSection.js
+++ src/components/companyProfile/ReviewSection.js
@@ -69,7 +69,7 @@ const AddNewReviewForm = (props) => {
             required
           />
           <div className="form-row mr-3 justify-content-end">
-            <button className="btn form-rounded r-green-button">Submit</button>
+            <button className="btn form-rounded r-green-button">Add</button>
           </div>
         </form>
       </div>
